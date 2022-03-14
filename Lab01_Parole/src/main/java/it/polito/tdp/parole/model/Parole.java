97df5dd87d1fcc3aeadd49fe908c8b4a183b753f
marchi <marchi@localhost>
2022-03-14 14:46:28 +0100

    classe Parole

--- Lab01_Parole/src/main/java/it/polito/tdp/parole/model/Parole.java
+++ Lab01_Parole/src/main/java/it/polito/tdp/parole/model/Parole.java
@@ -1,24 +1,44 @@
 package it.polito.tdp.parole.model;
 
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
-public class Parole {
+public class Parole{
+	
+	Map<String, String> mappa;
+	List<String> lista;
 		
 	public Parole() {
 		//TODO
+		mappa = new TreeMap<String, String>();
+		lista = new LinkedList<String>();
 	}
 	
 	public void addParola(String p) {
 		//TODO
+		mappa.put(p, p);
 	}
 	
 	public List<String> getElenco() {
 		//TODO
-		return null;
+		for(String s: mappa.values())
+		{
+			lista.add(s);
+		}
+		
+		return lista;
 	}
 	
 	public void reset() {
 		// TODO
+		mappa.clear();
+		lista.clear();
 	}
+
+	
+	
+	
 
 }
